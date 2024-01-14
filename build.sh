156b60eba7fafb155cec143f77f55ce09af06e75
parmanand007 <parmanand.p@brimo.in>
2024-01-14 18:32:49 +0530

    changes in settings for deployment

---
+++ build.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+#Build project
+
+echo "Building the project..."
+
+python3.12 -m pip install -r requirements.txt
+
+echo "Make Migrations"
+
+python3.12 manage.py makemigrations --noinput
+
+python3.12 manage.py migrate --noinput
+
+echo "Collect Static"
+
+python3.12 manage.py collectstatic --noinput --clear
